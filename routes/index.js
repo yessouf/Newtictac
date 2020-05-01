fdc4de75aff130588c7affee400ff4e2fc8126ae
Yessouf Yacouba <y.yacouba@hotmail.fr>
2020-05-01 10:13:13 +0200

    index

--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,11 @@
 var express = require('express');
 var router = express.Router();
-<<<<<<< HEAD
-
-const mongoose = require('mongoose');
-
-// useNewUrlParser ;)
-var options = {
-  connectTimeoutMS: 5000,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
- };
-
-// --------------------- BDD -----------------------------------------------------
-mongoose.connect('mongodb+srv://nico:22662266@cluster0-i1rij.mongodb.net/test?retryWrites=true&w=majority',
-   options,
-   function(err) {
-    if (err) {
-      console.log(`error, failed to connect to the database because --> ${err}`);
-    } else {
-      console.info('*** Database Ticketac connection : Success ***');
-    }
-   }
-);
-
-var journeySchema = mongoose.Schema({
-  departure: String,
-  arrival: String,
-  date: Date,
-  departureTime: String,
-  price: Number,
-});
-
-var journeyModel = mongoose.model('journey', journeySchema);
-
-var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
-var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
-
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('lastTrip',);
-});
-
-
-router.get('/homepage', function(req, res, next) {
-  res.render('homepage',);
-=======
 var request = require('sync-request');
 var userModel = require('../models/users')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('login',);
->>>>>>> 8ef71c38e5c8bc3fed812d0bc870b9577c83da16
 });
 
 router.get('/homePage', async function(req, res, next){
