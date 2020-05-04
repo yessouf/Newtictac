b3f14eacefe59e6e5c21853df6d9da9c990c7600
yessouf <63399152+yessouf@users.noreply.github.com>
2020-05-04 02:59:57 +0200

    Update journey.js

--- models/journey.js
+++ models/journey.js
@@ -8,9 +8,8 @@ var journeySchema = mongoose.Schema({
     price: Number,
   });
   
-  var journeyModel = mongoose.model('journey', journeySchema);
-  
-  var city = ["Paris","Marseille","Nantes","Lyon","Rennes","Melun","Bordeaux","Lille"]
-  var date = ["2018-11-20","2018-11-21","2018-11-22","2018-11-23","2018-11-24"]
 
-  module.exports = journeyModel;
+  
+ 
+
+module.exports = mongoose.model('journey', journeySchema);
